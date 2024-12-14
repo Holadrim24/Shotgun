17a7c9f738495b78dff1f5c4fdac9332cfb755fe
Patrik Lundgren <holadrim2487@gmail.com>
2024-12-14 20:23:11 +0100

    Update Player.cs

--- TheShotgunGame/Classes/Player.cs
+++ TheShotgunGame/Classes/Player.cs
@@ -12,8 +12,7 @@ namespace TheShotgunGame.Classes
     {
         public FormShotgun form;
         public ActionOutcomes actionOutcomes;
-
-        //Konstruktor
+        
         public Player(FormShotgun form)
         {
             this.form = form;
@@ -23,16 +22,11 @@ namespace TheShotgunGame.Classes
         {
             this.actionOutcomes = actionOutcomes;  
         }
-
-        //Skapar en lokal variabel som visar hur många skott man har så man kan använda det för att avgöra om man kan använda
-        //Shotgun i PlayerLogic-metoden
         public int playerAmmo
         {
             get { return ammo; }
             set { ammo = value; }
         }
-
-        //Gör en override på Ammo-metoden som ärvts från GameBase så den kan användas i denna klassen
         public override void Ammo()
         {
             if (ammo > 0)
@@ -41,59 +35,44 @@ namespace TheShotgunGame.Classes
             }
         }
  
-        //Detta skickar det nuvarande DisplayAmmo för player till labeln i FormShotgun
         public override void DisplayAmmo()
         {
             form.PlayerAmmoLabel.Text = $"{ammo}";
         }
 
-        //Detta är logiken för spelaren, den anropar button-click-events i FormShotgun
-        //Sen kollar den vilket val man gjorde (vilken knapp man tryckt på)
         public string PlayerLogic(string playerAction)
         {
             Debug.WriteLine("PlayerLogic called with action: " + playerAction);
             string lastPlayerMove = playerAction;
 
-            //Om senaste moven var "Skjut" så kommer det tas bort ett skott i räknaren för spelaren och sen uppdateras det i labeln på formuläret
             if (lastPlayerMove == "Skjut")
             {          
                 RemoveAmmo(1);
                 DisplayAmmo();
             }
-            //Om senaste moven var "Ladda" så kommer det läggas till ett skott i räknaren
             else if (lastPlayerMove == "Ladda")
             {
                 AddAmmo(1);
                 DisplayAmmo();
             }
-            //Om senaste move var "Block" så fortsätter bara variabeln vidare mot ActionOutcomes-klassen i slutet
             else if (lastPlayerMove == "Blocka")
             {
                 
             }
-            //Man ska bara kunna trycka på Shotgun-knappen om man har 3 skott
             else if (lastPlayerMove == "Shotgun")
             {
-                //Om spelaren har 3 eller fler skott så kan man använda Shotgun
                 if (playerAmmo >= 3)
                 {
                     return lastPlayerMove;
                 }
-                //Annars visas detta meddelandet i en MessageBox
                 else
                 {
                     MessageBox.Show("Du kan bara använda Shotgun om du har 3 skott");
                     return null;
                 }
             }
-
-
                 return lastPlayerMove;
-
-         
-
         }
-
     }
 }
 
